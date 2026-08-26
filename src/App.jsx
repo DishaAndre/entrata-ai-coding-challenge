4e156c5758dfd57dd12cb0b9a6e8222f0b505a83
DishaAndre <andredisha55@gmail.com>
2026-08-26 16:36:40 +0530

    chore: remove default template assets and unused boilerplate CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import ProfileForm from './task1/ProfileForm';
+import CacheDemo from './task2/CacheDemo';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-50 py-12 px-4">
+    <div className="min-h-screen bg-gray-50 py-8 px-4 space-y-8">
       <ProfileForm />
+      <CacheDemo />
     </div>
   );
 }
